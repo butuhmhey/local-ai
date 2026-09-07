674ed7cec45c33a8d77f3ec349330c690201fd3c
butuhmhey <kasper.kal@proton.me>
2026-09-07 21:38:31 +0000

    Add and change stuff

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o46vdofa2u"
+    "revision": "0.j6q1bnqrbn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
